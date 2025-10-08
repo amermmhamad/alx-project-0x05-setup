d4f9451fdc0315aa1fe245cd60b7d9843df0d2e8
Amer <amermmhamad@gmail.com>
2025-10-08 04:14:25 +0300

    Added custom hook

--- alx-project-0x13/pages/index.tsx
+++ alx-project-0x13/pages/index.tsx
@@ -24,9 +24,11 @@ const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
+        <h1 className="text-black text-4xl font-bold mb-2">
+          Generate your dumbass picture
+        </h1>
         <p className="text-lg text-gray-700 mb-4">
-          Generate stunning images based on your prompts!
+          Give me your dumbassest prompt.. bitch.
         </p>
 
         <div className="w-full max-w-md">
@@ -34,8 +36,13 @@ const Home: React.FC = () => {
             type="text"
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleGenerateImage();
+              }
+            }}
             placeholder="Enter your prompt here..."
-            className="w-full p-3 border border-gray-300 rounded-lg mb-4"
+            className="text-black w-full p-3 border border-gray-300 rounded-lg mb-4"
           />
           <button
             onClick={handleGenerateImage}
